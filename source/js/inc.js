fbbb2921e5b37b385c90e5a74ac19d195e053301
Maksym Chukhriaiev <MChukhriaiev@on.seo>
2017-05-26 20:12:07 +0300

    prefixfree enabled

--- source/js/inc.js
+++ source/js/inc.js
@@ -1,5 +1,13 @@
 (new function ($R) {
 
+  /*[include src="../lib/prefixfree.js"]*/;
+
+  onBoot(function(){
+    $R.on('componentCreate', function(e){
+      StyleFix.styleElement(e.target.options.stylesheet);
+    });
+  });
+
   /*[include src="../lib/swipe.js"]*/
   $R.Swipe = this.Swipe;
   $R.menu = $R.value(/*[include src="/menu.json"]*/);
